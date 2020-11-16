48d9aca2687af8e980ac36bf4052c308239779cd
Berkay Ugur Senocak <berkayacademic@gmail.com>
2020-11-16 15:35:33 +0300

    Number numbers are now generated within a range provided by user

--- generate.sh
+++ generate.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+  echo -e "\nUsage:\t$0 MIN MAX\n"
+  exit 1
+fi
+
+DIFF=$(($2-$1+1))
 RANDOM=$$
 for f in {a..z}{0..99}; do
     for i in `seq 10`; do
-      echo $RANDOM >> "$f.txt"
+      R=$(($(($RANDOM%$DIFF))+$1))
+      echo $R >> "$f.txt"
     done
 done

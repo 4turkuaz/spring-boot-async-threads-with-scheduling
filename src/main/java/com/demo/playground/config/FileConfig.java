9e687b865e69428589ddad156260f90c37477829
Berkay Ugur Senocak <berkayacademic@gmail.com>
2020-11-16 16:13:54 +0300

    Correcting path

--- src/main/java/com/demo/playground/config/FileConfig.java
+++ src/main/java/com/demo/playground/config/FileConfig.java
@@ -1,8 +1,7 @@
 package com.demo.playground.config;
 
 public class FileConfig {
-    private static final String username = System.getProperty("user.name");
-    private static final String path = System.getProperty("user.dir");
-    public static final String INPUT_DIR = "/home/" + username + path + "/fileDir";
-    public static final String OUTPUT_DIR = "/home/" + username + path + "newFileDir";
+    protected static final String path = System.getProperty("user.dir");
+    public static final String INPUT_DIR = path + "/fileDir";
+    public static final String OUTPUT_DIR = path + "newFileDir";
 }

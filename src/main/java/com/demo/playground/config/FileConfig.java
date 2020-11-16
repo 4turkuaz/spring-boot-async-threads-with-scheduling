fe3cc939845b8daf5143ae768ac1787a594c86ad
Berkay Ugur Senocak <berkayacademic@gmail.com>
2020-11-16 16:05:21 +0300

    Generalising path

--- src/main/java/com/demo/playground/config/FileConfig.java
+++ src/main/java/com/demo/playground/config/FileConfig.java
@@ -2,6 +2,7 @@ package com.demo.playground.config;
 
 public class FileConfig {
     private static final String username = System.getProperty("user.name");
-    public static final String INPUT_DIR = "/home/" + username + "/Desktop/fileDir";
-    public static final String OUTPUT_DIR = "/home/" + username + "/Desktop/newFileDir";
+    private static final String path = System.getProperty("user.dir");
+    public static final String INPUT_DIR = "/home/" + username + path + "/fileDir";
+    public static final String OUTPUT_DIR = "/home/" + username + path + "newFileDir";
 }

bda0265a55d58bf817256badde801665dbbbb744
Berkay Ugur Senocak <berkayacademic@gmail.com>
2020-11-16 15:26:33 +0300

    Improving generator script

--- generate.sh
+++ generate.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-num=1
+RANDOM=$$
 for f in {a..z}{0..99}; do
-    echo $num > "$f.txt"
-    echo $((num+1)) >> "$f.txt"
-    echo $((num+2)) >> "$f.txt"
-    echo $((num+3)) >> "$f.txt"
-    ((num=num+1))
+    for i in `seq 10`; do
+      echo $RANDOM >> "$f.txt"
+    done
 done

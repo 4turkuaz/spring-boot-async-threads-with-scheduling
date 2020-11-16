3a39a7c2b99d689544302c59496aeee72e22f36a
Berkay Ugur Senocak <berkayacademic@gmail.com>
2020-11-16 16:17:06 +0300

    Update README.md

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ files named {a..z}{0..99} (e.g. a7, z99) and puts 10 random integers with the gi
 mkdir fileDir && mv generate.sh fileDir && cd fileDir
 chmod 655 generate.sh 
 ./generate.sh <MIN_LIMIT> <MAX_LIMIT> # choose limits as you wish
-rm generate.sh
+mv generate.sh ..
 cd ..
 ```
 
--- src/main/java/com/demo/playground/config/FileConfig.java
+++ src/main/java/com/demo/playground/config/FileConfig.java
@@ -3,5 +3,5 @@ package com.demo.playground.config;
 public class FileConfig {
     protected static final String path = System.getProperty("user.dir");
     public static final String INPUT_DIR = path + "/fileDir";
-    public static final String OUTPUT_DIR = path + "newFileDir";
+    public static final String OUTPUT_DIR = path + "/newFileDir";
 }
